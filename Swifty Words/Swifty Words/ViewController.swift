ed5d1702b16e4a68f8d75351620a7ba293893e5c
Ruben Sissing <ruben@sissing.nl>
2018-08-23 15:02:49 +0100

    feat(swifty-words): implement first level and reading level file logic

--- Swifty Words/Swifty Words/ViewController.swift
+++ Swifty Words/Swifty Words/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameplayKit
 
 final class ViewController: UIViewController {
 
@@ -23,7 +24,43 @@ final class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		self.loadLevel()
+	}
+
+	private func loadLevel() {
+		var clueString = ""
+		var solutionString = ""
+		var letterBits = [String]()
+
+		if let levelFilePath = Bundle.main.path(forResource: "level\(self.level)", ofType: "txt"), let levelContents = try? String(contentsOfFile: levelFilePath) {
+			var lines = levelContents.components(separatedBy: "\n")
+			lines = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: lines) as! [String]
+
+			for (index, line) in lines.enumerated() {
+				let parts = line.components(separatedBy: ": ")
+				let answer = parts[0]
+				let clue = parts[1]
+
+				clueString += "\(index + 1). \(clue)\n"
+				let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+				solutionString += "\(solutionWord.count) letters\n"
+				self.solutions.append(solutionWord)
+
+				let bits = answer.components(separatedBy: "|")
+				letterBits += bits
+			}
+		}
+
+		self.cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+		self.answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+		letterBits = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: letterBits) as! [String]
+
+		if letterBits.count == self.letterButtons.count {
+			for i in 0 ..< letterBits.count {
+				self.letterButtons[i].setTitle(letterBits[i], for: .normal)
+			}
+		}
 	}
 
 	@IBAction private func submitTapped(_ sender: Any) {
