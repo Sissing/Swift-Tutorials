31f4d34fbcce72df38fa3714edaf022ce5c56141
Ruben Sissing <ruben@sissing.nl>
2018-08-23 14:15:59 +0100

    feat(swift-words): connect storyboard outlets to view controller

--- Swifty Words/Swifty Words/ViewController.swift
+++ Swifty Words/Swifty Words/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+	@IBOutlet private var cluesLabel: UILabel!
+	@IBOutlet private var answersLabel: UILabel!
+	@IBOutlet private var currentAnswerInput: UITextField!
+	@IBOutlet private var scoreLabel: UILabel!
+	@IBOutlet private var letterButtons: [UIButton]!
+
+	private var activatedButtons = [UIButton]()
+	private var solutions = [String]()
+	private var score = 0
+	private var level = 1
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
+	@IBAction private func submitTapped(_ sender: Any) {
+	}
+
+	@IBAction private func clearTapped(_ sender: Any) {
+	}
+
+	@IBAction private func letterTapped(_ sender: Any) {
+	}
 
 }
 
