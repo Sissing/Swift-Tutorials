d88e4f496a8a223cf7b8ac3fb4120cee65f50984
Ruben Sissing <ruben@sissing.nl>
2018-09-03 16:23:35 +0100

    feat(names-faces): setup image picker

--- Names to Faces/Names to Faces/ViewController.swift
+++ Names to Faces/Names to Faces/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UICollectionViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
 	}
 
 	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -22,5 +22,44 @@ class ViewController: UICollectionViewController {
 	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 		return self.collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
 	}
+
+	@IBAction func userDidTapNewPerson(_ sender: Any) {
+		let picker = UIImagePickerController()
+		picker.allowsEditing = true
+		picker.delegate = self
+		self.present(picker, animated: true)
+	}
 }
 
+extension ViewController: UIImagePickerControllerDelegate {
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+		guard let image = info[.editedImage] as? UIImage else {
+			return
+		}
+
+		let imageName = UUID().uuidString
+		let imagePath = self.getDocumentsDirectory()?.appendingPathComponent(imageName)
+
+		do {
+			if let jpegData = image.jpegData(compressionQuality: 80), let imagePath = imagePath {
+				try jpegData.write(to: imagePath)
+			} else {
+				print("Error(Image): error with compression or impage path unwrapping")
+			}
+		}
+		catch {
+			print("Error(Image Write): \(error)")
+		}
+
+		self.dismiss(animated: true)
+	}
+
+	private func getDocumentsDirectory() -> URL? {
+		let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+		return paths.first
+	}
+}
+
+extension ViewController: UINavigationControllerDelegate {
+
+}
