addf9b92d0ace67a37684f1091b5cc6e9a5b7d8f
Ruben Sissing <ruben@sissing.nl>
2018-10-23 14:29:36 +0100

    feat(flo): draw plus button on screen

---
+++ Flo/Flo/PushButton.swift
@@ -0,0 +1,67 @@
+//
+//  PushButton.swift
+//  Flo
+//
+//  Created by Ruben Sissing on 23/10/2018.
+//  Copyright © 2018 Sissing.nl. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class PushButton: UIButton {
+
+	private struct Constants {
+		static let plusLineWidth: CGFloat = 3.0
+		static let plusButtonScale: CGFloat = 0.6
+		static let halfPointShift: CGFloat = 0.5
+	}
+
+	private var halfWidth: CGFloat {
+		return self.bounds.width / 2
+	}
+
+	private var halfHeight: CGFloat {
+		return self.bounds.height / 2
+	}
+
+	override func draw(_ rect: CGRect) {
+		let path = UIBezierPath(ovalIn: rect)
+		UIColor.blue.setFill()
+		path.fill()
+
+		let plusWidth: CGFloat = min(self.bounds.width, self.bounds.height) * Constants.plusButtonScale
+		let halfPlusWidth = plusWidth / 2
+
+		let plusPath = UIBezierPath()
+
+		plusPath.lineWidth = Constants.plusLineWidth
+
+		// horizontal line
+		plusPath.move(to: CGPoint(
+			x: self.halfWidth - halfPlusWidth + Constants.halfPointShift,
+			y: self.halfHeight + Constants.halfPointShift
+		))
+
+		plusPath.addLine(to: CGPoint(
+			x: self.halfWidth + halfPlusWidth + Constants.halfPointShift,
+			y: self.halfHeight + Constants.halfPointShift
+		))
+
+		// vertical line
+		plusPath.move(to: CGPoint(
+			x: self.halfWidth + Constants.halfPointShift,
+			y: self.halfHeight - halfPlusWidth + Constants.halfPointShift
+		))
+
+		plusPath.addLine(to: CGPoint(
+			x: self.halfWidth + Constants.halfPointShift,
+			y: self.halfHeight + halfPlusWidth + Constants.halfPointShift
+		))
+
+		UIColor.white.setStroke()
+
+		plusPath.stroke()
+	}
+
+}
