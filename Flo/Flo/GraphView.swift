523f813a32b29d52b6bc76fab1c29703107c3c09
Ruben Sissing <ruben@sissing.nl>
2018-10-26 16:28:59 +0100

    refactor(flo): simplify draw function

--- Flo/Flo/GraphView.swift
+++ Flo/Flo/GraphView.swift
@@ -23,67 +23,83 @@ import UIKit
 	}
 
 	private var graphPoints = [4, 2, 6, 4, 5, 8, 3]
-
+	private var drawRect = CGRect.zero
 
 	override func draw(_ rect: CGRect) {
+		self.drawRect = rect
 		let path = UIBezierPath(roundedRect: rect, byRoundingCorners: .allCorners, cornerRadii: Constants.cornerRadiusSize)
 		path.addClip()
 
 		let context = UIGraphicsGetCurrentContext()!
-		let colors = [self.startColor.cgColor, self.endColor.cgColor] as CFArray
-
-		let colorSpace = CGColorSpaceCreateDeviceRGB()
-		let colorLocations: [CGFloat] = [0.0, 1.0]
-
-		let gradient = CGGradient(colorsSpace: colorSpace, colors: colors, locations: colorLocations)!
 
 		let startPoint = CGPoint.zero
 		let endPoint = CGPoint(x: 0, y: self.bounds.height)
-		context.drawLinearGradient(gradient, start: startPoint, end: endPoint, options: [])
-
-		let graphWidth = rect.width - Constants.margin * 2 - 4
-		let columnXPoint = { (column: Int) -> CGFloat in
-			let spacing = graphWidth / CGFloat(self.graphPoints.count - 1)
-			return CGFloat(column) * spacing + Constants.margin + 2
-		}
-
-		let maxValue = graphPoints.max()!
-		let graphHeight = rect.height - Constants.topBorder - Constants.bottomBorder
-		let columnYPoint = { (graphPoint: Int) -> CGFloat in
-			let y = CGFloat(graphPoint) / CGFloat(maxValue) * graphHeight
-			return graphHeight + Constants.topBorder - y
-		}
+		context.drawLinearGradient(self.backgroundGradient(), start: startPoint, end: endPoint, options: [])
 
 		UIColor.white.setFill()
 		UIColor.white.setStroke()
 
 		let graphPath = UIBezierPath()
-		graphPath.move(to: CGPoint(x: columnXPoint(0), y: columnYPoint(self.graphPoints[0])))
-
-		for i in 1..<self.graphPoints.count {
-			let nextPoint = CGPoint(x: columnXPoint(i), y: columnYPoint(self.graphPoints[i]))
-			graphPath.addLine(to: nextPoint)
-		}
+		self.setGraphPoints(graphPath: graphPath)
 
 		context.saveGState()
 
-		let clippingPath = graphPath.copy() as! UIBezierPath
+		self.addClippingPath(graphPath: graphPath)
 
-		clippingPath.addLine(to: CGPoint(x: columnXPoint(self.graphPoints.count - 1), y: rect.height))
-		clippingPath.addLine(to: CGPoint(x: columnXPoint(0), y: rect.height))
-
-		clippingPath.addClip()
-
-		let graphStartPoint = CGPoint(x: Constants.margin, y: columnYPoint(maxValue))
+		let graphStartPoint = CGPoint(x: Constants.margin, y: self.columnYPoint(graphPoint: self.graphPoints.max()!))
 		let graphEndPoint = CGPoint(x: Constants.margin, y: self.bounds.height)
-		context.drawLinearGradient(gradient, start: graphStartPoint, end: graphEndPoint, options: [])
+		context.drawLinearGradient(self.backgroundGradient(), start: graphStartPoint, end: graphEndPoint, options: [])
 		context.restoreGState()
 
 		graphPath.lineWidth = 2.0
 		graphPath.stroke()
 
+		self.drawColumnPointDots()
+	}
+
+	private func backgroundGradient() -> CGGradient {
+		let colors = [self.startColor.cgColor, self.endColor.cgColor] as CFArray
+
+		let colorSpace = CGColorSpaceCreateDeviceRGB()
+		let colorLocations: [CGFloat] = [0.0, 1.0]
+
+		return CGGradient(colorsSpace: colorSpace, colors: colors, locations: colorLocations)!
+	}
+
+	private func columnXPoint(column: Int) -> CGFloat {
+		let graphWidth = self.drawRect.width - Constants.margin * 2 - 4
+
+		let spacing = graphWidth / CGFloat(self.graphPoints.count - 1)
+		return CGFloat(column) * spacing + Constants.margin + 2
+	}
+
+	private func columnYPoint(graphPoint: Int) -> CGFloat {
+		let graphHeight = self.drawRect.height - Constants.topBorder - Constants.bottomBorder
+		let y = CGFloat(graphPoint) / CGFloat(self.graphPoints.max()!) * graphHeight
+		return graphHeight + Constants.topBorder - y
+	}
+
+	private func setGraphPoints(graphPath: UIBezierPath) {
+		graphPath.move(to: CGPoint(x: self.columnXPoint(column: 0), y: self.columnYPoint(graphPoint: self.graphPoints[0])))
+
+		for i in 1..<self.graphPoints.count {
+			let nextPoint = CGPoint(x: self.columnXPoint(column: i), y: self.columnYPoint(graphPoint: self.graphPoints[i]))
+			graphPath.addLine(to: nextPoint)
+		}
+	}
+
+	private func addClippingPath(graphPath: UIBezierPath) {
+		let clippingPath = graphPath.copy() as! UIBezierPath
+
+		clippingPath.addLine(to: CGPoint(x: self.columnXPoint(column: self.graphPoints.count - 1), y: self.drawRect.height))
+		clippingPath.addLine(to: CGPoint(x: self.columnXPoint(column: 0), y: self.drawRect.height))
+
+		clippingPath.addClip()
+	}
+
+	private func drawColumnPointDots() {
 		for i in 0..<self.graphPoints.count {
-			var point = CGPoint(x: columnXPoint(i), y: columnYPoint(self.graphPoints[i]))
+			var point = CGPoint(x: self.columnXPoint(column: i), y: columnYPoint(graphPoint: self.graphPoints[i]))
 			point.x -= Constants.circleDiameter / 2
 			point.y -= Constants.circleDiameter / 2
 
