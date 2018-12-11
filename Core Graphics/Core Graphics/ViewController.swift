3791f40439049872f344142fbc0ea29fd70c301d
Ruben Sissing <ruben@sissing.nl>
2018-12-11 09:55:53 +0000

    feat(core-graphics): draw rectangle

--- Core Graphics/Core Graphics/ViewController.swift
+++ Core Graphics/Core Graphics/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet private var imageView: UIImageView!
+
+	private var currentDrawType = 0
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		self.drawRectangle()
 	}
 
+	private func drawRectangle() {
+		let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+		let image = renderer.image { context in
+			let rectangle = CGRect(origin: .zero, size: context.currentImage.size)
+			context.cgContext.setFillColor(UIColor.red.cgColor)
+			context.cgContext.setStrokeColor(UIColor.black.cgColor)
+			context.cgContext.setLineWidth(10)
+			context.cgContext.addRect(rectangle)
+			context.cgContext.drawPath(using: .fillStroke)
+		}
+		self.imageView.image = image
+	}
+
+	@IBAction func redrawTapped(_ sender: Any) {
+		self.currentDrawType += 1
+
+		if self.currentDrawType > 5 {
+			self.currentDrawType = 0
+		}
+
+		switch currentDrawType {
+		case 0:
+			self.drawRectangle()
+		default:
+			break
+		}
+	}
 
 }
 
