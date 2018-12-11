66b49e074f73b382aefe7aec57f8f42f53df56cb
Ruben Sissing <ruben@sissing.nl>
2018-12-11 09:17:42 +0000

    chore(core-graphics): project setup

---
+++ Core Graphics/Core Graphics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core Graphics
+//
+//  Created by Ruben Sissing on 11/12/2018.
+//  Copyright © 2018 Fressh.nl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
