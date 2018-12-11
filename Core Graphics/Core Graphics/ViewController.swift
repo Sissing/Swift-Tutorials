a5571783e34ddf7d680762d9f156fb586f8bd581
Ruben Sissing <ruben@sissing.nl>
2018-12-11 10:04:04 +0000

    feat(core-graphics): draw circle

--- Core Graphics/Core Graphics/ViewController.swift
+++ Core Graphics/Core Graphics/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
 	@IBOutlet private var imageView: UIImageView!
 
+	private let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+	private let rectangle = CGRect(x: 5, y: 5, width: 502, height: 502)
 	private var currentDrawType = 0
 
 	override func viewDidLoad() {
@@ -21,13 +23,24 @@ class ViewController: UIViewController {
 	}
 
 	private func drawRectangle() {
-		let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
-		let image = renderer.image { context in
-			let rectangle = CGRect(origin: .zero, size: context.currentImage.size)
+		let image = self.renderer.image { context in
+			let rectangle = self.rectangle
 			context.cgContext.setFillColor(UIColor.red.cgColor)
 			context.cgContext.setStrokeColor(UIColor.black.cgColor)
 			context.cgContext.setLineWidth(10)
 			context.cgContext.addRect(rectangle)
+			context.cgContext.drawPath(using: .fillStroke)
+		}
+		self.imageView.image = image
+	}
+
+	private func drawCircle() {
+		let image = self.renderer.image { context in
+			let rectangle = self.rectangle
+			context.cgContext.setFillColor(UIColor.red.cgColor)
+			context.cgContext.setStrokeColor(UIColor.black.cgColor)
+			context.cgContext.setLineWidth(10)
+			context.cgContext.addEllipse(in: rectangle)
 			context.cgContext.drawPath(using: .fillStroke)
 		}
 		self.imageView.image = image
@@ -43,6 +56,8 @@ class ViewController: UIViewController {
 		switch currentDrawType {
 		case 0:
 			self.drawRectangle()
+		case 1:
+			self.drawCircle()
 		default:
 			break
 		}
