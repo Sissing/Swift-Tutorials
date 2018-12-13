5b83557758375940a1624127d13e2e0eada911bf
Ruben Sissing <ruben@sissing.nl>
2018-12-13 20:25:26 +0000

    chore(exploding-monkeys): initial project setup

---
+++ Exploding Monkeys/Exploding Monkeys/BuildingNode.swift
@@ -0,0 +1,14 @@
+//
+//  BuildingNode.swift
+//  Exploding Monkeys
+//
+//  Created by Ruben Sissing on 13/12/2018.
+//  Copyright © 2018 Fressh.nl. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class BuildingNode: SKSpriteNode {
+
+}
---
+++ Exploding Monkeys/Exploding Monkeys/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Exploding Monkeys
+//
+//  Created by Ruben Sissing on 13/12/2018.
+//  Copyright © 2018 Fressh.nl. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+enum CollisionType: UInt32 {
+	case banana = 1
+	case building = 2
+	case player = 4
+}
+
+class GameScene: SKScene {
+    
+}
