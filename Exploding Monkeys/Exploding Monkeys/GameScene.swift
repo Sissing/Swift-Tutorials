85d6700af60ca9e79e9caad2f9e125f4d09c03de
Ruben Sissing <ruben@sissing.nl>
2018-12-16 21:54:11 +0000

    feat(exploding-monkeys): add launch method

--- Exploding Monkeys/Exploding Monkeys/GameScene.swift
+++ Exploding Monkeys/Exploding Monkeys/GameScene.swift
@@ -28,6 +28,7 @@ class GameScene: SKScene {
 		self.backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
 
 		self.createBuildings()
+		self.createPlayers()
 	}
 
 	private func createBuildings() {
@@ -45,7 +46,41 @@ class GameScene: SKScene {
 	}
 
 	func launch(angle: Int, velocity: Int) {
-		
+		let speed = Double(velocity) / 10.0
+		let radians = self.degreesToRadians(angle)
+
+		if self.banana != nil {
+			self.banana.removeFromParent()
+			self.banana = nil
+		}
+
+		self.banana = SKSpriteNode(imageNamed: "banana")
+		self.banana.name = "banana"
+		self.banana.physicsBody = SKPhysicsBody(circleOfRadius: self.banana.size.width / 2)
+		self.banana.physicsBody?.categoryBitMask = CollisionType.banana.rawValue
+		self.banana.physicsBody?.collisionBitMask = CollisionType.building.rawValue | CollisionType.player.rawValue
+		self.banana.physicsBody?.contactTestBitMask = CollisionType.building.rawValue | CollisionType.player.rawValue
+		self.banana.physicsBody?.usesPreciseCollisionDetection = true
+		self.addChild(self.banana)
+
+		if self.currentPlayer == 1 {
+			self.createSequence(for: self.player1, isPlayer1: true, speed: speed, radians: radians)
+		} else {
+			self.createSequence(for: self.player2, isPlayer1: false, speed: speed, radians: radians)
+		}
+
+	}
+
+	private func createSequence(for player: SKSpriteNode, isPlayer1: Bool, speed: Double, radians: Double) {
+		self.banana.position = CGPoint(x: isPlayer1 ? player.position.x - 30 : player.position.x + 30, y: player.position.y + 40)
+		self.banana.physicsBody?.angularVelocity = isPlayer1 ? -20 : 20
+		let raiseArm = SKAction.setTexture(SKTexture(imageNamed: isPlayer1 ? "player1Throw" : "player2Throw"))
+		let lowerArm = SKAction.setTexture(SKTexture(imageNamed: "player"))
+		let pause = SKAction.wait(forDuration: 0.15)
+		let sequence = SKAction.sequence([raiseArm, pause, lowerArm])
+		player.run(sequence)
+		let impulse = CGVector(dx: cos(radians), dy: sin(radians) * speed)
+		self.banana.physicsBody?.applyImpulse(impulse)
 	}
 
 	func createPlayers() {
@@ -73,5 +108,9 @@ class GameScene: SKScene {
 		self.player2.position = CGPoint(x: player2Building.position.x, y: player2Building.position.y + ((player2Building.size.height + self.player2.size.height) / 2))
 
 		self.addChild(self.player2)
+	}
+
+	private func degreesToRadians(_ degrees: Int) -> Double {
+		return Double(degrees) * Double.pi / 180.0
 	}
 }
